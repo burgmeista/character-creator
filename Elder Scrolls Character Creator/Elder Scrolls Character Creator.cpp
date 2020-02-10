1089e1285524e6d8c2cc146f1ab6207ab08a9dff
burgmeista <lucas44488@gmail.com>
2020-02-10 00:26:55 -0700

    Added a bit more code to get more of the main function working.

--- Elder Scrolls Character Creator/Elder Scrolls Character Creator.cpp
+++ Elder Scrolls Character Creator/Elder Scrolls Character Creator.cpp
@@ -2,13 +2,34 @@
 
 using namespace std;
 
-class Character
+class Character3
 {
 public:
+    // character sheet
+    string name;
+    string sex;
+    string race;
+    string profession;
+    string birthsign;
 
+    // roleplaying choices
+    string faction1;
+    string faction2;
+    string hometown;
 };
 
 int main()
 {
-    cout << "Hello World!\n";
+    string factions[10] = {"Great Houses", "Vampires", "Religions", "Fighter's Guild", "Mage's Guild"};
+
+    cout << "Please enter the number of the Elder Scrolls game.\n";
+    int game;
+    cin >> game;
+
+    if (game == 3)
+    {
+        cout << "Please enter the number of the Elder Scrolls game.\n";
+        int game;
+        cin >> game;
+    }
 }
